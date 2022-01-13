7550c124fcf9c565fc0cd327553e2774d9196534
Huy Vo <vohuy819@gmail.com>
2022-01-13 12:34:00 +1030

    add save and render function for local storage

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const description= document.querySelector(".description");
 const container= document.querySelector(".container")
 let hour= moment().hour(9)
 console.log(hour.format("hA"));
-schedule = [{
-    activityIndex: 1,
-    saved: "hello"
-}];
+schedule = [];
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
@@ -16,26 +13,46 @@ currentDay.textContent = moment().format("dddd, MMM YYYY");
 // THEN I am presented with timeblocks for standard business hours
 let time= moment(hour)
 for (var i =0; i < 9; i++) {
-    div = document.createElement("div");
-    div.className = "row w-100";
-    div.setAttribute("data-index", i);
-    h2 = document.createElement("h2");
-    h2.textContent = time.format("hA");
-    time = hour.add(1,"hours")
-    h2.className = "hour col-2";
-    // WHEN I click into a timeblock
-    // THEN I can enter an event
-    textArea= document.createElement("textarea");
-    textArea.className = "col-9 description";
-    button= document.createElement("button");
-    button.innerHTML = '<i class="fas fa-save"></i>';
-    button.className = "saveBtn col-1";
-    container.appendChild(div)
-    div.appendChild(h2)
-    div.appendChild(textArea)
-    div.appendChild(button)
-
+  div = document.createElement("div");
+  div.className = "row w-100";
+  div.setAttribute("data-index", i);
+  h2 = document.createElement("h2");
+  h2.textContent = time.format("hA");
+  time = hour.add(1,"hours")
+  h2.className = "hour col-2";
+  // WHEN I click into a timeblock
+  // THEN I can enter an event
+  textArea= document.createElement("textarea");
+  textArea.className = "col-9 description";
+  schedule.push(i);
+  button= document.createElement("button");
+  button.innerHTML = '<i class="fas fa-save"></i>';
+  button.className = "saveBtn col-1";
+  container.appendChild(div)
+  div.appendChild(h2)
+  div.appendChild(textArea)
+  div.appendChild(button)
+  
 }
+// WHEN I refresh the page
+// THEN the saved events persist
+// Get stored todos from localStorage
+    var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
+  
+    // If todos were retrieved from localStorage, update the todos array to it
+    if (storedSchedule !== null) {
+      schedule = storedSchedule;
+      console.log(schedule)
+    }
+  
+    // This is a helper function that will render todos to the DOM
+    // for (var i =0; schedule.length; i++) {
+      
+     var render = document.querySelector(`[data-index="${0}"]`);
+     var renderText= render.querySelector('textarea');
+     renderText.textContent = schedule[0];
+// }
+  
 // WHEN I view the timeblocks for that day
 
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
@@ -51,32 +68,16 @@ container.addEventListener("click", function(event) {
       // Get its data-index value 
       var index = element.parentElement.getAttribute("data-index");
       console.log(element.parentElement.querySelector("textarea").value)
-      var activity = {
-          activityIndex: index,
-          saved : element.parentElement.querySelector("textarea").value
+      var activity = element.parentElement.querySelector("textarea").value
+      schedule[index] = activity
       }
-      schedule.push(activity);
       // Store updated activities in localStorage
       storeActivity();
     }
-  });
+  );
 
   function storeActivity() {
     localStorage.setItem("schedule", JSON.stringify(schedule));
-  }
-// WHEN I refresh the page
-// THEN the saved events persist
-function init() {
-    // Get stored todos from localStorage
-    var storedSchedule = JSON.parse(localStorage.getItem("schedule"));
-  
-    // If todos were retrieved from localStorage, update the todos array to it
-    if (storedSchedule !== null) {
-      schedule = storedSchedule;
-    }
-  
-    // This is a helper function that will render todos to the DOM
-    renderSchedules();
   }
 
 function renderSchedules() {
